0f60e5f0f920ed19d92692a7ae3e947bb59508b5
absentees <scottblissett@gmail.com>
2017-07-29 12:17:15 +1000

    works

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,12 @@ import Hashids from 'hashids';
 $(document).ready(function () {
 
 	var hashids = new Hashids('mrpoopybutthole', 10, 'abcdefghijklmnopqrstuvwxyz1234567890');
-	console.log(hashids.encode(0));
+	var $nextLink = $('.next-link');
 
+	var collection = hashids.decode($nextLink.attr('data-id'));
+
+	var next = Math.round(Math.random()*collection[1]);
+	console.log(next);
+
+	$nextLink.attr('href', '/' + hashids.encode(next, collection[1]));
 });
